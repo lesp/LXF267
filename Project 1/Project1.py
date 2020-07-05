e8abc17d8e0ba7bbfe5aece0e85b9a1c32517175
Les Pounder <lespounder@gmail.com>
2020-07-05 20:27:08 +0100

    fixed typo

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -5,7 +5,6 @@ from time import sleep
 while True:
     if eh.touch.five.is_pressed() == True:
         print("Hailing frequencies open")
-        #webbrowser.open_new_tab("https://intl.startrek.com/")
-        webbrowser.open_new_tab("https://meet.jit.si/BlackpoolMakerspace28-3-20")
+        webbrowser.open_new_tab("https://intl.startrek.com/")
         sleep(0.5)
 
